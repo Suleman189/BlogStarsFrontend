5f631f0fbf3bec72a20c79e234000e7e1a924556
Suleman <suleman.bscs.189@gmail.com>
2024-12-13 21:52:15 +0500

    Modified vite configurations for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   server: {
     port:  4000
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
   }
 })
